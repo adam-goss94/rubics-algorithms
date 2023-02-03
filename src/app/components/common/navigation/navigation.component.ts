0cc01455791160a0972a9a112b8a84ac5dfbd7e0
Adam Goss <adam.goss94@gmail.com>
2023-02-03 14:11:54 +0100

    Added translations

--- src/app/components/common/navigation/navigation.component.ts
+++ src/app/components/common/navigation/navigation.component.ts
@@ -1,33 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
+
 export class NavigationComponent implements OnInit {
-  items = [
-    { title: 'Home', route: '/home', active: false },
+  public items = [
+    { title: this.translate('header_home'), route: '/home', active: false },
     { title: '3x3', route: '/3x3', active: false },
     { title: '4x4', route: '/4x4', active: false },
     { title: 'Megamix', route: '/megamix', active: false },
   ];
 
-  constructor() {}
+  constructor(private translateService: TranslateService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     const selectedItem = localStorage.getItem('selectedItem');
     this.items.forEach(item => {
       if (item.title === selectedItem) {
         item.active = true;
       }
     });
+
   }
 
   onNavItemClick(item: { active: boolean; title: string; }) {
     this.items.forEach(i => i.active = false);
     item.active = true;
     localStorage.setItem('selectedItem', item.title);
+  }
+
+  public  translate(key: string) {
+    return this.translateService.instant(key)
   }
 }
